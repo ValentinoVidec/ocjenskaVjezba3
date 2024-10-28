518927d1efa8e9c524c905cceddb71f9f86e2687
ValentinoVidec <valentinovidec1505@gmail.com>
2024-10-28 12:51:20 +0100

    Add files via upload

---
+++ ocjenskaVjezba3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ocjenskaVjezba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite rečenicu:");
+            string input = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (words.Length > 0)
+                {
+                    string firstWord = words[0];
+                    string lastWord = words[words.Length - 1];
+
+                    Console.WriteLine($"Prva riječ: {firstWord}");
+                    Console.WriteLine($"Zadnja riječ: {lastWord}");
+                }
+                else
+                {
+                    Console.WriteLine("Rečenica ne sadrži riječi.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Niste unijeli ništa.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
